670d98d33a30a655f2ce0cd9a83b50698ef428e6
may2250 <may2250@163.com>
2014-03-13 15:24:58 +0800

    add badges img

--- server/models/User.js
+++ server/models/User.js
@@ -7,8 +7,7 @@ var User
     , GoogleStrategy = require('passport-google').Strategy
     , LinkedInStrategy = require('passport-linkedin').Strategy
     , check =           require('validator').check
-    , userRoles =       require('../../client/js/routingConfig').userRoles,
-	, userDao =       require('/models/UserDao');
+    , userRoles =       require('../../client/js/routingConfig').userRoles;
 
 var users = [
     {
